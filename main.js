a332a244a04ed17e662782690d0aa0db5f19275e
Wonjun Choi <wonjune12@hotmail.com>
2018-12-10 17:53:41 -0500

    fixed for loop

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
 
-// const axios = require('axios');
-
-// var container = document.querySelector(".container");
 var row = document.querySelector(".row");
 let prom = axios.get("https://www.redbullshopus.com/products.json");
 
-
-var index=0;
-
-
-
 prom.then((response)=>{
 
-    var productArr = response.data.products;
     for (let index = 0; index < response.data.products.length; index++){
+
+    var productArr = response.data.products;
+            //creating column
             let divCol = document.createElement("div");        
             divCol.classList.add("col-12");
             divCol.classList.add("col-md-3");
+
+            //creating card
             let divCard = document.createElement("div");
             divCard.classList.add("card");
+
+            //creating image
             let img = document.createElement("img");
             img.classList.add("card-img-top");
+
+            //creating description
             let cardBody = document.createElement("div");
             cardBody.classList.add("card-body");
+
+            //adding to the box
             row.appendChild(divCol);
             divCol.appendChild(divCard);
             divCard.appendChild(img);
             divCard.appendChild(cardBody);
 
 
-            let cardImg = document.querySelectorAll(".card-img-top")[index];
+            let cardImg = document.querySelectorAll("img")[index];
             let description = document.querySelectorAll(".card-body")[index];
             let price = productArr[index].variants[0].price;
             let size = productArr[index].options[0].values;
             cardImg.src=productArr[index].images[0].src;
-            // let color = productArr[index].options[1].values;
-            description.innerHTML = `\$ ${price}  <br/> Size : ${size}`
+            description.innerHTML = `\$ ${price}  <br/> Size : ${size}`;
     
         }
    
