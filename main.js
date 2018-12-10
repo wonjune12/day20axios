b1de100dc3134c61bc7c1f10b312ac81dc7b85cc
Wonjun Choi <wonjune12@hotmail.com>
2018-12-10 17:26:36 -0500

    fixed error

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // const axios = require('axios');
 
 // var container = document.querySelector(".container");
-// var row = document.querySelector(".row");
+var row = document.querySelector(".row");
 let prom = axios.get("https://www.redbullshopus.com/products.json");
 
 
@@ -14,7 +14,6 @@ prom.then((response)=>{
 
     var productArr = response.data.products;
     for (let index = 0; index < response.data.products.length; index++){
-         if (productArr[index].variants[0].available == true){
             let divCol = document.createElement("div");        
             divCol.classList.add("col-12");
             divCol.classList.add("col-md-3");
@@ -36,10 +35,9 @@ prom.then((response)=>{
             let size = productArr[index].options[0].values;
             cardImg.src=productArr[index].images[0].src;
             // let color = productArr[index].options[1].values;
-            description.innerHTML = `\$ ${price}  <br/> Size : ${size} `
+            description.innerHTML = `\$ ${price}  <br/> Size : ${size}`
     
         }
-     }
    
 
 })
